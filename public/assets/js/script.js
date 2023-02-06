d34aeed8445fb223f2c4df7a010f1bffdae6f68b
Perrine <perrine.morsch@gmail.com>
2023-02-06 21:51:12 +0100

    suite

---
+++ public/assets/js/script.js
@@ -0,0 +1,6 @@
+const about = document.getElementById('about');
+const skills = document.getElementById('skills');
+const journey = document.getElementById('journey');
+const hobbies = document.getElementById('hobbies');
+const portfolio = document.getElementById('portfolio');
+const contact = document.getElementById('contact');
